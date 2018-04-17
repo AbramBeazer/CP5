ddf958de47c8bad41ef2d01f746319579ff400ef
Abram Milo Beazer <abram@droplet.abram>
2018-04-17 22:03:40 +0000

    3rd commit, adding drag/drop, more vue integration

--- public/script.js
+++ public/script.js
@@ -5,6 +5,10 @@ var app = new Vue({
     movies: [],
     games: [],
     token: "",
+    drag: {},
+    bookText: "",
+    movieText: "",
+    gameText: "",
   },
   created: function() {
 //do login stuff, if successful, do getters.
@@ -15,6 +19,9 @@ var app = new Vue({
   },
   filters: {
     
+  },
+  computed: {
+  
   },
   methods: {
     getBooks: function() {
@@ -34,10 +41,77 @@ var app = new Vue({
     },
     clearBooks: function() {
       axios.delete("api/books/" + this.token).then(response => {
-        this.getTickets();
+        this.getBooks();
       	return true;
       }).catch(err => {
       });
+    },
+    addBook: function() {
+      
+      this.getBooks();
+    },
+    getMovies: function() {
+      axios.get("api/movies/" + this.token).then(response => {
+	this.movies = response.data;
+	return true;
+      }).catch(err => {
+      });
+    },
+    saveMovies: function() {
+      axios.post("api/movies/" + this.token, {
+	movies: this.movies,
+      }).then(response => {
+	return true;
+      }).catch(err => {
+      });
+    },
+    clearMovies: function() {
+      axios.delete("api/movies/" + this.token).then(response => {
+        this.getMovies();
+      	return true;
+      }).catch(err => {
+      });
+    },
+    addMovie: function() {
+
+      this.getMovies();
+    },
+    getGames: function() {
+      axios.get("api/games/" + this.token).then(response => {
+	this.games = response.data;
+	return true;
+      }).catch(err => {
+      });
+    },
+    saveGames: function() {
+      axios.post("api/games/" + this.token, {
+	games: this.games,
+      }).then(response => {
+	return true;
+      }).catch(err => {
+      });
+    },
+    clearGames: function() {
+      axios.delete("api/games/" + this.token).then(response => {
+        this.getGames();
+      	return true;
+      }).catch(err => {
+      });
+    },
+    addGame: function() {
+      
+      this.getGames();
+    },
+    dragItem: function(item) {
+      this.drag = item;
+    },
+    dropItem: function(container, item) {
+   /*
+      let indexTarget = container.indexOf(item);
+      let index = container.indexOf(this.drag);
+      container.splice(index,1);
+      container.splice(indexTarget,0,this.drag);
+    */
     },
   }
 });
--- server.js
+++ server.js
@@ -14,12 +14,62 @@ const env = process.env.NODE_ENV || 'development';
 const config = require('./knexfile')[env];  
 const db = require('knex')(config);
 
+const jwt = require('jsonwebtoken');
+let jwtSecret = process.env.jwtSecret;
+if (jwtSecret === undefined) {
+  console.log("You need to define a jwtSecret environment variable to continue.");
+  knex.destroy();
+  process.exit();
+}
+
+
+/* login stuff
+
+    if (result) {
+       let token = jwt.sign({ id: user.id }, jwtSecret, {
+        expiresIn: 86400 // expires in 24 hours
+       });
+      res.status(200).json({user:{username:user.username,name:user.name,id:user.id},token:token});
+    } else {
+       res.status(403).send("Invalid credentials");
+    }
+
+*/
+
+/*
+ let token = jwt.sign({ id: user.id }, jwtSecret, {
+     expiresIn: 86400 // expires in 24 hours
+    });
+    res.status(200).json({user:user,token:token});
+
+*/
+
+const verifyToken = (req, res, next) => {
+  const token = req.headers['authorization'];
+  if (!token)
+    return res.status(403).send({ error: 'No token provided.' });
+  jwt.verify(token, jwtSecret, function(err, decoded) {
+    if (err)
+      return res.status(500).send({ error: 'Failed to authenticate token.' });
+    // if everything good, save to request for use in other routes
+    req.userID = decoded.id;
+    next();
+  });
+}
+
 app.get
 
 app.post
 
-app.delete
-
+app.delete('/api/books/:token', verifyToken, (req,res) => {
+ // id of the person who is following
+   let token = parseInt(req.params.token);
+ // check this id
+   if (token !== req.token) {
+     res.status(403).send();
+     return;
+   }
+});
 
 
 app.listen(4000, () => console.log('Server listening on port 4000!'))
